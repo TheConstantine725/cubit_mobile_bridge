028f1ed2b220744b93a87c81b89f7b4fe7ffc351
Konstantinos Dimos <konstantinos.dimos@themart.gr>
2025-07-11 10:24:07 +0300

    pyarrow generator issues adressed

--- src/_source_extractor.py
+++ src/_source_extractor.py
@@ -2,11 +2,11 @@ import asyncio
 from sqlalchemy import create_engine, Connection, Engine, text
 import pendulum 
 from typing import Optional, Any
-from _limits import __safe_threading_limit
-from _configs import BridgeConfigs
-from source_extractor import QueryReader, engine_creator, generate_data
 from pathlib import Path
 from json import dump
+from ._limits import __safe_threading_limit
+from ._configs import BridgeConfigs
+from .source_extractor import QueryReader, engine_creator, generate_source_data, transform_final_data
 
 SAFE_THREADING_LIMIT = __safe_threading_limit()
 
@@ -15,17 +15,15 @@ SOURCE_EXTRACTION_CONFIG = BridgeConfigs().generate_extraction_config()
 def _generate_semaphore(limit: int = SAFE_THREADING_LIMIT) -> asyncio.Semaphore:
     return asyncio.Semaphore(limit)
 
-async def main():
-    semaphore = _generate_semaphore()
-    pass    
+# async def main():
+#     _generate_semaphore() #type: ignore
+#     pass    
 
-if __name__ == "__main__":
+
+def main():
     source_1 = SOURCE_EXTRACTION_CONFIG["dev"]
-    print(query := QueryReader().read_source_extraction(source_1["query_alias"]))
-    print(engine := engine_creator(source_1["connection_string"],))
-    sql_result = generate_data(engine,query).test_pyarrow_table()
-    _path = Path.cwd().joinpath("output")
-    _path.mkdir(exist_ok=True)
-    import daft
-    
-    print(daft.from_arrow(sql_result))
+    (query := QueryReader().read_source_extraction(source_1["query_alias"]))
+    (engine := engine_creator(source_1["connection_string"],))
+    print("\n\n")
+    sql_result = generate_source_data(engine,query, extraction_format="pyarrow")
+    return sql_result 
--- src/source_extractor/data_collector.py
+++ src/source_extractor/data_collector.py
@@ -1,17 +1,22 @@
-from sqlalchemy import Engine, Connection, Row, CursorResult
-from typing import Any, Optional, Sequence, Self, Iterable, Generator, Literal, Union
+from sqlalchemy import Engine,  Row, CursorResult
+from typing import Any, Optional, Sequence,  Generator, Literal, Union
+from itertools import repeat
 import pyarrow
+import numpy as np
 
 TRANSFROMATION_FORMATS = Literal["pyarrow","json"]
+EXTRACTED_RESULTS = Union[pyarrow.Table, list[dict[str,Any]], None]
 
 class SqlResult:
 
     def __init__(self, cursor_result: CursorResult) -> None:
         self.column_keys: Sequence[str]= [key.lower() for key in cursor_result.keys()]
         self.rows: Generator[Sequence[Row[Any]]] = self.__generate_results(cursor_result)
+        # self.__base_pydict = 
 
-    def __generate_results(self, _cursor_res: CursorResult) -> Generator[Sequence[Row[Any]]]:
-        yield _cursor_res.fetchall()
+
+    def __generate_results(self, _cursor_res: CursorResult) -> Generator[Sequence[Row[Any]],Any, None]:
+        yield from _cursor_res
 
     def transpose_to_json(self,) -> list[dict[str,Any]]:
         result = []
@@ -21,25 +26,29 @@ class SqlResult:
             for i, key in enumerate(self.column_keys):
                 temp[key] = row[i]
             result.append(temp)
-
         return result
 
-    def test_pyarrow_table(self,)->pyarrow.Table:
-        result = {}
-        for i, column_name in enumerate(self.column_keys):
-            array = []
-            for row in self.rows:
-                array.append(row[i])
-            result[column_name] = pyarrow.array(array)
+    def test_pyarrow_table(self,)-> Any:
+        __base_pydict = {col:[] for col in self.column_keys}
+        for row in self.rows:
+            for col, r in zip(self.column_keys, row):
+                __base_pydict[col].append(r)
         
-        return pyarrow.table(result)
+        for col_name, col_data in __base_pydict.items():
+            __base_pydict[col_name] = pyarrow.array(np.array(col_data))
+        return pyarrow.table(__base_pydict)
+        
+        
+
+                
+            
 
 
 def generate_source_data(engine: Engine,
                   query: str,*,
                   execution_options_kwargs: Optional[dict[str, Any]] = None,
                   extraction_format: Literal["pyarrow", "json"]
-                  ) -> Optional[SqlResult]:
+                  ) -> Union[list[dict[str,Any]], pyarrow.Table, None]:
     if execution_options_kwargs is None:
         try:
             with engine.connect() as _conn:
@@ -47,7 +56,7 @@ def generate_source_data(engine: Engine,
         except Exception as SqlError:
             print(f"There was an error in the execution of the SQL Query.\n{'='*40}\n{SqlError}\n{'='*40}")
         else:
-            return SqlResult(result)
+            return transform_final_data(SqlResult(result), extraction_format)
 
     elif isinstance(execution_options_kwargs, dict):
         try:
@@ -56,7 +65,7 @@ def generate_source_data(engine: Engine,
         except Exception as SqlError:
             print(f"There was an error in the execution of the SQL Query.\n{'='*40}\n{SqlError}\n{'='*40}")
         else:
-            return SqlResult(result)
+            return transform_final_data(SqlResult(result), extraction_format)
     else:
         raise ValueError(f"Cannot assign {SqlResult:=}")
     
